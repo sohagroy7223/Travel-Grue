a40e0729d0af4639c21f0f487bd6b942a9dc40e9
sohagroy7223 <freelancersohagroy@gmail.com>
2026-04-08 15:43:10 +0600

    addad navlink active status and some change

--- src/Page/Hotel.jsx
+++ src/Page/Hotel.jsx
@@ -4,14 +4,14 @@ import { FaStar } from "react-icons/fa";
 const Hotel = ({ hotel }) => {
   console.log(hotel);
   return (
-    <div className="md:flex   space-y-3  items-center gap-3 ">
+    <div className="md:flex mt-5  space-y-2  items-center gap-3 ">
       {/* img */}
       <img
-        className="w-full md:w-120 h-48 bg-cover rounded-lg "
+        className="w-full md:w-120 h-48 mt-2 bg-cover rounded-lg "
         src={hotel.image}
         alt=""
       />
-      <div className="space-y-2 border  p-4 rounded-2xl">
+      <div className="space-y-2 border p-4 rounded-lg">
         <h3 className="text-2xl font-bold">{hotel.title}</h3>
         <div className="flex text-base-200 gap-3.5 ">
           <p>{hotel.guests}Guests</p>
@@ -21,11 +21,11 @@ const Hotel = ({ hotel }) => {
         </div>
         <p>Air conditional kitchen</p>
         <p>Cancellation fixiblelity available</p>
-        <div className="flex items-center justify-between px-5">
+        <div className="flex items-center justify-between px-4">
           <p className="flex items-center gap-1.5">
             {" "}
             <FaStar className="text-yellow-500"></FaStar>
-            {hotel.rating}
+            {hotel.rating}(12)
           </p>
           <p>${hotel.price_per_night}/par Night</p>
         </div>
